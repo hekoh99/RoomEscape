1be6b88143e190baa4652299eba544deca054fa2
hekoh99 <hekoh99@naver.com>
2020-09-10 00:04:32 +0900

    advanced function

--- RoomEscape.cpp
+++ RoomEscape.cpp
@@ -8,9 +8,29 @@ int main() {
 	
 	ScenePtr scene1 = Scene::create("1번 방", "images/배경-1.png");
 	ScenePtr scene2 = Scene::create("2번 방", "images/배경-2.png");
+	ScenePtr scene3 = Scene::create("3번 방", "images/배경-3.png");
 
 	auto key = Object::create("images/열쇠.png", scene1, 600, 150);
 	auto door1 = Object::create("images/문-오른쪽-닫힘.png", scene1, 800, 270);
+	auto orange = Object::create("images/주황이.png", scene1, 900, 100);
+	auto star1 = Object::create("images/별.png", scene1, 900, 100, false);
+	auto star2 = Object::create("images/별.png", scene2, 900, 100);
+	auto star3 = Object::create("images/별.png", scene3, 900, 100);
+	star1->setScale(0.5f);
+	star2->setScale(0.5f);
+	star3->setScale(0.5f);
+	star1->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
+		star1->pick();
+		return true;
+		});
+	star2->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
+		star2->pick();
+		return true;
+		});
+	star3->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
+		star3->pick();
+		return true;
+		});
 
 	key->setScale(0.3f);
 	key->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
@@ -19,6 +39,30 @@ int main() {
 		});
 	auto vase = Object::create("images/화분.png", scene1, 550, 150);
 	vase->setScale(1.2f);
+
+	auto apple = Object::create("images/사과.png", scene1, 550, 180);
+	apple->setScale(0.4f);
+
+	apple->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
+		apple->pick();
+		return true;
+		});
+	bool ate = false;
+	orange->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
+
+		if (apple->isHanded()) {
+			apple->drop();
+			apple->hide();
+			orange->setImage("images/웃는-주황이.png");
+			star1->show();
+			ate = true;
+		}
+		else if(ate == false){
+			showMessage("배고파!!");
+		}
+		return true;
+		});
+
 	vase->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
 		if (action == MouseAction::MOUSE_DRAG_LEFT && is_moved==false) {
 			vase->locate(scene1, 450, 150);
@@ -38,6 +82,8 @@ int main() {
 		}
 		else if(key->isHanded()){
 			door1->setImage("images/문-오른쪽-열림.png");
+			key->drop();
+			key->hide();
 			open1 = true;
 		}
 		else {
@@ -48,6 +94,7 @@ int main() {
 
 	auto door2 = Object::create("images/문-왼쪽-열림.png", scene2, 320, 270);
 	auto door3 = Object::create("images/문-오른쪽-닫힘.png", scene2, 910, 270);
+	auto door4 = Object::create("images/문-왼쪽-열림.png", scene3, 290, 290);
 
 	door2->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
 		scene1->enter();
@@ -66,15 +113,10 @@ int main() {
 	box->setScale(0.2f);
 	auto hint = Object::create("images/암호.png", scene2, 400, 150, false);
 
-	auto escape = Object::create("images/탈출.png", scene2, 0, 0,false);
-	auto end = Object::create("images/끝내기.png", scene2, 510, 200, false);
-	end->setScale(0.4f);
-
 	door3->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
 
 		if (open3 == true) {
-			escape->show();
-			end->show();
+			scene3->enter();
 		}
 		else if (is_locked == false && open3 == false) {
 			open3 = true;
@@ -95,7 +137,7 @@ int main() {
 	
 	keypad->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
 
-		showKeypad("BANGTAL", door3);
+		showKeypad("AAAAAB", door3);
 
 		return true;
 	});
@@ -123,8 +165,141 @@ int main() {
 		return true;
 	});
 
+
+	door4->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
+		scene2->enter();
+		return true;
+		});
+
+	auto area1 = Object::create("images/투명위치.png", scene3, 680, 400);
+	area1->setScale(0.5f);
+	auto area2 = Object::create("images/투명위치.png", scene3, 860, 620);
+	area2->setScale(0.5f);
+	auto area3 = Object::create("images/투명위치.png", scene3, 1030, 340);
+	area3->setScale(0.5f);
+
+	bool fill1 = false;
+	bool fill2 = false;
+	bool fill3 = false;
+
+	int collect = 0;
+	auto hole = Object::create("images/구멍.png", scene3, 450, 100, false);
+	area1->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
+		if (fill1==false && star1->isHanded()) {
+			fill1 = true;
+			star1->drop();
+			star1->setScale(0.8f);
+			star1->locate(scene3, 650, 370);
+			collect++;
+			if (collect == 3) {
+				hole->show();
+			}
+		}
+		else if (fill1 == false && star2->isHanded()) {
+			fill1 = true;
+			star2->drop();
+			star2->setScale(0.8f);
+			star2->locate(scene3, 650, 370);
+			collect++;
+			if (collect == 3) {
+				hole->show();
+			}
+		}
+		else if (fill1 == false && star3->isHanded()) {
+			fill1 = true;
+			star3->drop();
+			star3->setScale(0.8f);
+			star3->locate(scene3, 650, 370);
+			collect++;
+			if (collect == 3) {
+				hole->show();
+			}
+		}
+		return true;
+		});
+
+	area2->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
+		if (fill2 == false && star1->isHanded()) {
+			fill2 = true;
+			star1->drop();
+			star1->setScale(0.8f);
+			star1->locate(scene3, 830, 590);
+			collect++;
+			if (collect == 3) {
+				hole->show();
+			}
+		}
+		else if (fill2 == false && star2->isHanded()) {
+			fill2 = true;
+			star2->drop();
+			star2->setScale(0.8f);
+			star2->locate(scene3, 830, 590);
+			collect++;
+			if (collect == 3) {
+				hole->show();
+			}
+		}
+		else if (fill2 == false && star3->isHanded()) {
+			fill2 = true;
+			star3->drop();
+			star3->setScale(0.8f);
+			star3->locate(scene3, 830, 590);
+			collect++;
+			if (collect == 3) {
+				hole->show();
+			}
+		}
+		return true;
+		});
+
+	area3->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
+		if (fill3 == false && star1->isHanded()) {
+			fill3 = true;
+			star1->drop();
+			star1->setScale(0.8f);
+			star1->locate(scene3, 1000, 310);
+			collect++;
+			if (collect == 3) {
+				hole->show();
+			}
+		}
+		else if (fill3 == false && star2->isHanded()) {
+			fill3 = true;
+			star2->drop();
+			star2->setScale(0.8f);
+			star2->locate(scene3, 1000, 310);
+			collect++;
+			if (collect == 3) {
+				hole->show();
+			}
+		}
+		else if (fill3 == false && star3->isHanded()) {
+			fill3 = true;
+			star3->drop();
+			star3->setScale(0.8f);
+			star3->locate(scene3, 1000, 310);
+			collect++;
+			if (collect == 3) {
+				hole->show();
+			}
+		}
+		return true;
+		});
+
+	auto escape = Object::create("images/탈출.png", scene3, 0, 0, false);
+	auto end = Object::create("images/끝내기.png", scene3, 510, 200, false);
+	end->setScale(0.4f);
 	end->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
 		endGame();
+		return true;
+		});
+
+	hole->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
+		escape->show();
+		end->show();
+		star1->hide();
+		star2->hide();
+		star3->hide();
 		return true;
 		});
 
